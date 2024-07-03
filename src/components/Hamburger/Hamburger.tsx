0aa1b7cbbbc2f6ebce9efb73b20d8a854c4c5816
gastón_Alonso() <tonga88@live.com.ar>
2024-07-03 00:18:34 -0300

    err visual de prod

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -14,13 +14,13 @@ export const Hamburger = () => {
       } md:flex  md:translate-x-0 md:static md:bg-transparent md:items-center`}
     >
       <button
-        className="bg-white p-4 w-8/12 text-start font-semibold text-gray-600 text-xl md:hidden"
+        className="bg-white p-4 w-[231px] text-start font-semibold text-gray-600 text-xl md:hidden md:w-full"
         onClick={() => dispatch(toggleMenu(menu))}
       >
         <IoClose className="text-gray hover:text-orange " />
       </button>
 
-      <ul className=" absolute left-0  z-20 bg-white h-full p-4 w-8/12 md:flex md:w-full md:bg-transparent md:static md:items-center md:[&>li]:mb-0 md:gap-8 md:[&>li]:text-DarkGrayishBlue md:[&>li]:font-medium md:[&>li]:h-[100px] md:[&>li]:flex md:[&>li]:items-center md:py-0 md:hover:[&>li]:border-b-4 md:[&>li]:border-orange hover:[&>li]:text-orange md:hover:[&>li]:text-VeryDarkBlue">
+      <ul className=" absolute left-0  z-20 bg-white h-full pl-4 pt-4 w-[321px] md:flex md:w-full md:bg-transparent md:static md:items-center md:[&>li]:mb-0 md:gap-8 md:[&>li]:text-DarkGrayishBlue md:[&>li]:font-medium md:[&>li]:h-[100px] md:[&>li]:flex md:[&>li]:items-center md:py-0 md:hover:[&>li]:border-b-4 md:[&>li]:border-orange hover:[&>li]:text-orange md:hover:[&>li]:text-VeryDarkBlue">
         <li className="mb-3 text-gray-dark font-semibold font-Kumbh text-sm">
           <a href="#">Collection</a>
         </li>
